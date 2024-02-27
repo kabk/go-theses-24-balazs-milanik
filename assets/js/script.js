0e327f1b672f6476f39dfc56269945f95a14289b
Balázs Milánik <balazs.milanik@gmail.com>
2024-02-27 10:53:52 +0100

    Update_2024–02–27

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,11 +42,9 @@ $(document).ready(function () {
     const door1 = document.getElementById("door_1");
     const door2 = document.getElementById("door_2");
 
-    // Slide doors into view
     door1.style.left = "0";
     door2.style.right = "0";
 
-    // After 5 seconds, slide doors back to initial position
     setTimeout(() => {
       door1.style.left = "-50vw";
       door2.style.right = "-50vw";
@@ -82,3 +80,81 @@ function togglediv() {
   var div = document.getElementById("map");
   div.style.display = div.style.display == "block" ? "none" : "block";
 }
+
+function toggleDescription(index) {
+    var descriptions = document.querySelectorAll('.main .description');
+    var description = descriptions[index - 1];
+    description.classList.toggle("reveal");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Function to load SVG files into a side div
+  function loadSVGsIntoSide(sideDiv, svgFiles) {
+      // Directory path for the SVG files
+      const directoryPath = "assets/graphics/Assets/";
+
+      // Get the side index from the data-side-index attribute
+      const sideIndex = parseInt(sideDiv.dataset.sideIndex);
+
+      // Check if sideIndex is a number
+      if (isNaN(sideIndex)) {
+          console.error('Invalid data-side-index attribute:', sideDiv.dataset.sideIndex);
+          return;
+      }
+
+      // Calculate the start index for SVG files
+      const startIndex = sideIndex * 5;
+
+      // Array to hold loaded SVG filenames for this side
+      const loadedSVGs = [];
+
+      // Load each SVG file into the side div
+      for (let i = 1; i < 10; i++) {
+          const index = startIndex + i;
+          if (index >= svgFiles.length) break;
+
+          const svgPath = directoryPath + svgFiles[index];
+
+          // Check if the SVG file has already been loaded into this side
+          if (loadedSVGs.includes(svgFiles[index])) {
+              console.log(`SVG file ${svgFiles[index]} already loaded into side ${sideIndex}. Skipping.`);
+              continue;
+          }
+
+          // Create an object element to embed the SVG
+          const svgObject = document.createElement('object');
+          svgObject.type = 'image/svg+xml';
+          svgObject.data = svgPath;
+
+          // Apply styles to the SVG object
+          svgObject.style.maxHeight = '5vh';
+          svgObject.style.position = 'sticky';
+          svgObject.style.top = '0';
+
+          // Append the SVG object to the side div
+          sideDiv.appendChild(svgObject);
+
+          // Add the loaded SVG filename to the loadedSVGs array
+          loadedSVGs.push(svgFiles[index]);
+      }
+  }
+
+  // Get all "side" divs
+  const sideDivs = document.querySelectorAll('.side');
+
+  // Array to hold the names of SVG files
+  const svgFiles = [];
+  for (let i = 900; i <= 1000; i++) {
+      svgFiles.push(`a${i}.svg`);
+  }
+
+  // Load SVG files into each side div
+  sideDivs.forEach((sideDiv, index) => {
+      // Set the data-side-index attribute for each side div
+      sideDiv.dataset.sideIndex = index;
+      // Load SVGs into the side div
+      loadSVGsIntoSide(sideDiv, svgFiles);
+  });
+});
+
+
